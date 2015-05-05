e941f6dd569eea1eca4ac04910b7ccb82e50b526
Mikko Östlund <mikost@server.fake>
2015-05-05 17:03:58 +0200

    Work in progress - test does't pass! src/test/java/pack/ClazzTest.java: Converted to an Arquillian test. pom.xml:   1) Added artifact with the Java EE 7 APIs provided by Wildfly 8.2.0.Final:     <dependency>       <groupId>org.jboss.spec</groupId>       <artifactId>jboss-javaee-7.0</artifactId>       <version>1.0.2.Final</version>       <type>pom</type>       <scope>provided</scope>     </dependency>
      2) Added the "Arquillian JUnit integration artifact", which makes it possible to compile Arquillian tests.
        <dependency>
          <groupId>org.jboss.arquillian.junit</groupId>
          <artifactId>arquillian-junit-container</artifactId>
          <scope>test</scope>
        </dependency>

--- src/test/java/pack/ClazzTest.java
+++ src/test/java/pack/ClazzTest.java
@@ -1,15 +1,33 @@
 package pack;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+@RunWith(Arquillian.class)
 public class ClazzTest {
 
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                         .addClass(Clazz.class)
+                         .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject Clazz cut;
+	
     @Test
     public void should_create_greeting() {
-	assertThat(new Clazz().getGreeting(), is("Hello, World!"));
-    }    
+        assertThat(cut.getGreeting(), is(equalTo("Hello, World!")));
+    }
 }
